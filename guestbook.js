e1733f04098fe4c76c410f810f7bf1fcaf3ad5ab
Hanlei <89024752+roomforyeesus@users.noreply.github.com>
2022-06-25 13:57:07 -0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://4c4d9696.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
